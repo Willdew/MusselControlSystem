43cafc085629d8813ed53d9ba04486e1c589c555
Willdew <william.brochner@copenhagenatomics.com>
2022-06-08 10:23:06 +0200

    Added style convention for the code

---
+++ CodeConvention.py
@@ -0,0 +1,19 @@
+# Description: Class for mussel objects, extends animal
+class Mussel(animal):
+
+    # Constructor
+    def __init__(self, length, name):
+        self.length = length
+        self.__name = name
+
+    # Description: Getter for name of mussel
+    # input: none
+    # output: name(string)
+    def getname(self):
+        return self.__name + "Big Cock"
+
+    # Description: Method for feeding the mussel
+    # input: food(string)
+    # output: none
+    def feed(self, food):
+        print("Feeding mussel" + food)
