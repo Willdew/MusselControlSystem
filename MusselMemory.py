62e180dda88ac8b1ce09c75f86354eb7fe6c3605
Bajers <augustweijers@gmail.com>
2022-06-10 14:30:51 +0200

    Added class sensor and led_control, for OD-measurements

--- MusselMemory.py
+++ MusselMemory.py
@@ -1,43 +1,33 @@
-#This file contains all classes and functions for the MusselControl program.
 import os
 import time
 import machine
 import statistics
 
-class ODSensor:
-    # Constructor
+class od_sensor:
+
     def __init__(self, pin):
         self.__pin = pin
-        self.PD = machine.ADC(machine.Pin(self.__pin))
+        self.__PD = machine.ADC(machine.Pin(self.pin))
         self.PD.atten(machine.ADC.ATTN_11DB)
         self.PD.width(machine.ADC.WIDTH_10BIT)
 
-    #Getter-method for pin
-    def get_pin(self):
-        return self.__pin
-
-    def measure_OD(self):
+    def read_od(self):
         big = []
         for _ in range(20):
-            big.append(self.PD.read)
-        a = statistics.mean(big)
-        return a
+            big.append(self.PD.read())
 
-class LED:
-    def __init__(self,pin):
+        return statistics.mean(big)
+
+
+class led_control:
+    def __init__(self, pin):
         self.__pin = pin
-        self.pwn = machine.ADC(machine.Pin(self.__pin))
-        self.pwm.duty(52)
-        self.pwm.freq(78000)
+        self.__pwn = machine.PWN(machine.Pin(self.__pin))
+        self.pwn.duty(52)
+        self.pwn.freq(78000)
 
-    #getter-method for pin
-    def get_pin(self):
-        return self.__pin
-
-    #setter for freq
-    def set_freq(self, f):
-        self.pwn.freq(f)
-
-    #setter for duty
-    def set_duty(self, d):
+    def set_duty(self,d):
         self.pwn.duty(d)
+
+    def set_freq(self,f):
+        self.pwn.freq(f)
