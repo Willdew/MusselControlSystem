cf67c9579770fb4d739bbf4e6f360ff786abedbb
Willdew <william.brochner@copenhagenatomics.com>
2022-06-11 15:03:45 +0200

    PWM removed and comments added to code

--- MusselMemory.py
+++ MusselMemory.py
@@ -1,21 +1,30 @@
-#This file contains all classes and functions for the MusselControl program.
+# This file contains all classes and functions for the MusselControl program.
 import os
 import time
 import machine
 import statistics
 
+
+# Description: This class is able to read the data of an OD sensor
 class ODSensor:
     # Constructor
+    # input: pin(int)
+    # output: none
     def __init__(self, pin):
         self.__pin = pin
         self.PD = machine.ADC(machine.Pin(self.__pin))
         self.PD.atten(machine.ADC.ATTN_11DB)
         self.PD.width(machine.ADC.WIDTH_10BIT)
 
-    #Getter-method for pin
+    # Description: This method gets the value of the pin of the sensor
+    # input: none
+    # output: value(int)
     def get_pin(self):
         return self.__pin
 
+    # Description: This method gets the measured value of the sensor
+    # input: none
+    # output: value(int)
     def measure_OD(self):
         big = []
         for _ in range(20):
@@ -23,21 +32,30 @@ class ODSensor:
         a = statistics.mean(big)
         return a
 
-class LED:
-    def __init__(self,pin):
-        self.__pin = pin
-        self.pwn = machine.ADC(machine.Pin(self.__pin))
-        self.pwm.duty(52)
-        self.pwm.freq(78000)
 
-    #getter-method for pin
+# Description: This class is able to control an LED
+class LED:
+    # Constructor
+    # input: pin(int)
+    # output: none
+    def __init__(self, pin):
+        self.__pin = pin
+        self.__LED = machine.Pin(self.__pin, machine.Pin.OUT)
+
+    # Description: This method gets the value of the pin of the sensor
+    # input: none
+    # output: value(int)
     def get_pin(self):
         return self.__pin
 
-    #setter for freq
-    def set_freq(self, f):
-        self.pwn.freq(f)
+    # Description: This method turns the LED on
+    # input: none
+    # output: none
+    def on(self):
+        self.__LED.value(1)
 
-    #setter for duty
-    def set_duty(self, d):
-        self.pwn.duty(d)
+    # Description: This method turns the LED off
+    # input: none
+    # output: none
+    def off(self):
+        self.__LED.value(0)
